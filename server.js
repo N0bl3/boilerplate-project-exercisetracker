47fc0b95bca8a8777ee1472d0cda6084c4463186
N0bl3 <alexandre.noble.pro@gmail.com>
2019-05-02 21:46:24 +0200

    Adding required routes

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const bodyParser = require('body-parser')
 const cors = require('cors')
 
 const mongoose = require('mongoose')
-mongoose.connect(process.env.MLAB_URI || 'mongodb://localhost/exercise-track' )
+mongoose.connect(process.env.MLAB_URI || 'mongodb://localhost/exercise-track')
 
 app.use(cors())
 
@@ -14,35 +14,49 @@ app.use(bodyParser.json())
 
 
 app.use(express.static('public'))
+
 app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/views/index.html')
-});
+    res.sendFile(__dirname + '/views/index.html')
+})
+app.get('api/exercise/users', (req, res) => {
+    res.status(404).send()
+})
+app.get('/api/exercise/log', (req, res) => {
+    res.status(404).send()
+})
+
+app.post('/api/exercise/new-user', (req, res) => {
+    res.status(501).send()
+})
+app.post('/api/exercise/add', (req, res) => {
+    res.status(501).send()
+})
 
 
 // Not found middleware
 app.use((req, res, next) => {
-  return next({status: 404, message: 'not found'})
+    return next({status: 404, message: 'not found'})
 })
 
 // Error Handling middleware
 app.use((err, req, res, next) => {
-  let errCode, errMessage
+    let errCode, errMessage
 
-  if (err.errors) {
-    // mongoose validation error
-    errCode = 400 // bad request
-    const keys = Object.keys(err.errors)
-    // report the first validation error
-    errMessage = err.errors[keys[0]].message
-  } else {
-    // generic or custom error
-    errCode = err.status || 500
-    errMessage = err.message || 'Internal Server Error'
-  }
-  res.status(errCode).type('txt')
-    .send(errMessage)
+    if (err.errors) {
+        // mongoose validation error
+        errCode = 400 // bad request
+        const keys = Object.keys(err.errors)
+        // report the first validation error
+        errMessage = err.errors[keys[0]].message
+    } else {
+        // generic or custom error
+        errCode = err.status || 500
+        errMessage = err.message || 'Internal Server Error'
+    }
+    res.status(errCode).type('txt')
+        .send(errMessage)
 })
 
 const listener = app.listen(process.env.PORT || 3000, () => {
-  console.log('Your app is listening on port ' + listener.address().port)
+    console.log('Your app is listening on port ' + listener.address().port)
 })
